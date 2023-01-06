3e017a2f0327083255d4033e61389501014d59f4
adarijani <ali.darijani.me@gmail.com>
2023-01-06 15:37:22 +0100

    backing kitty.conf tmux.conf

--- make_backup.sh
+++ make_backup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# saving all formula and casks in backup.sh and making it executable
 /opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' > ./backup.sh
 chmod +x ./backup.sh
 
-cp ~/.zshrc zshrc
-cp ~/.vimrc vimrc
+# backing up dot files that exist in the home directory
+cp ~/.zshrc ./zshrc
+cp ~/.vimrc ./vimrc
+cp ~/.tmux.conf ./tmux.conf
+cp ~/.config/kitty/kitty.conf ./kitty.conf
+
 
