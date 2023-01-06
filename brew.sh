004ad9717e4cce3b22f952f42659063913063988
adarijani <ali.darijani.me@gmail.com>
2023-01-06 14:37:39 +0100

    zdg

--- brew.sh
+++ brew.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' > ./brew_install_formula_silent.sh
+/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' > ./formula_backup.sh
+chmod +x ./formula_backup.sh
