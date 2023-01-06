4510533be8153cfa091cc78db879826c8dd74a42
adarijani <ali.darijani.me@gmail.com>
2023-01-06 16:51:13 +0100

    Update install_brew.sh

--- install_brew.sh
+++ install_brew.sh
@@ -1,4 +1,5 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/rknot/.zprofile
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/rknot/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
