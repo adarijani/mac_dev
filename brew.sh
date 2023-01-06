0b4d905555a50ac9bac3a09c119b6c876cee4578
adarijani <ali.darijani.me@gmail.com>
2023-01-06 16:29:07 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,4 +2,5 @@
     echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/rknot/.zprofile
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/rknot/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-echo "close and open the shell to use brew"
+RED='\033[0;31m'
+printf "${RED}CLOSE YOUR TERMINAL AND OPEN IT AGAIN TO USE BREW${NC}\n"
