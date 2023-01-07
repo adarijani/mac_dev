7bb5398c1f7a2f1bf18f0d04933fc04371dfac95
adarijani <ali.darijani.me@gmail.com>
2023-01-07 12:14:47 +0100

    oh-my-zsh backup

---
+++ aliases.zsh
@@ -0,0 +1,6 @@
+## lsd (lsDeluxe) aliases
+alias l="lsd"
+alias la="lsd -a"
+alias ll="lsd -l"
+alias lll="lsd -al"
+alias llll="lsd -ahlF"
--- make_backup.sh
+++ make_backup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # brew update; brew upgrade; brew cleanup
-brew update
-brew upgrade
-brew cleanup
+brew update >&/dev/null
+brew upgrade >&/dev/null
+brew cleanup >&/dev/null
 # saving all formula and casks in backup.sh and making it executable
-/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' > ./backup.sh
-chmod +x ./backup.sh
+/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' >./backup.sh
+chmod +x ./backup.sh >&/dev/null
 
 # backing up dot files that exist in the home directory
-cp -Rf ~/.oh-my-zsh ./.oh-my-zsh
-cp ~/.zshrc ./.zshrc
-cp ~/.vimrc ./.vimrc
-cp ~/.tmux.conf ./.tmux.conf
-cp ~/.config/kitty/kitty.conf ./.kitty.conf
-cp ~/.zprofile ./.zprofile
-
+# backing up oh-my-zsh files
+rm -rf .oh-my-zsh >&/dev/null
+mkdir .oh-my-zsh >&/dev/null
+mkdir .oh-my-zsh/custom >&/dev/null
+cp ~/.oh-my-zsh/custom/aliases.zsh ./.oh-my-zsh/custom/aliases.zsh >&/dev/null
+cp ~/.zshrc ./.zshrc >&/dev/null
+cp ~/.vimrc ./.vimrc >&/dev/null
+cp ~/.tmux.conf ./.tmux.conf >&/dev/null
+cp ~/.config/kitty/kitty.conf ./.kitty.conf >&/dev/null
+cp ~/.zprofile ./.zprofile >&/dev/null
