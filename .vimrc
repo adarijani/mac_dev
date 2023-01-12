1c2b2554910e3e337c67bcc2c2adcc9f19e8c3f2
adarijani <ali.darijani.me@gmail.com>
2023-01-12 12:54:50 +0100

    asf

--- .vimrc
+++
@@ -1,6 +0,0 @@
-syntax on
-set number
-set relativenumber
-set spell
-set background=dark
-set t_Co=256
---
+++ config.sh
@@ -0,0 +1,6 @@
+mv ./.vimrc $HOME/.vimrc
+mv ./.zshrc $HOME/.zshrc
+mv ./aliases.zsh $HOME/.oh-my-zsh/custom/aliases
+mv ./.kitty.conf $HOME/.kitty.conf
+mv ./.alacritty.yml $HOME/.alacritty.yml
+mv ./.tmux.conf $HOME/.tmux.conf
