446daae484bd2fe0908bea85d48fc8f81c679e37
adarijani <ali.darijani.me@gmail.com>
2023-01-12 14:11:46 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 cat ./.vimrc > $HOME/.vimrc
 cat ./.zshrc > $HOME/.zshrc
-cat ./aliases.zsh > $HOME/.oh-my-zsh/custom/aliases
+cat ./aliases.zsh > $HOME/.oh-my-zsh/custom/aliases.zsh
 cat ./.kitty.conf > $HOME/.config/kitty/kitty.conf
 cat ./.alacritty.yml > $HOME/.alacritty.yml
 cat ./.tmux.conf > $HOME/.tmux.conf
