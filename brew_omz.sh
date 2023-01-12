f9eafb1924fbfdaf833611863b5c3f3552830ef7
adarijani <ali.darijani.me@gmail.com>
2023-01-12 12:05:10 +0100

    exec zsh at the end

--- brew_omz.sh
+++ brew_omz.sh
@@ -2,15 +2,12 @@
 RED='\033[1;91m'   # Red
 GREEN='\033[1;92m' # Green
 # installing brew and its necessary components
-printf "${GREEN}INSTALLING BREW${NC}\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # setting path and stuff for brew to make it visible
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >$HOME/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
-exec zsh
-printf "${GREEN}FINISHED INSTALLING brew${NC}\n"
 # installing oh-my-zsh
-printf "${GREEN}INSTALLING oh-my-zsh${NC}\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >&/dev/null
-printf "${GREEN}FINISHED INSTALLING oh-my-zsh${NC}\n"
+printf "${GREEN}FINISHED INSTALLING brew and oh-my-zsh${NC}\n"
+exec zsh
