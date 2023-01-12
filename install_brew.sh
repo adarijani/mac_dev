0a5f1d95927c482f5fcd06a2b48e06ee3838335a
adarijani <ali.darijani.me@gmail.com>
2023-01-12 11:07:23 +0100

    change

--- install_brew.sh
+++ install_brew.sh
@@ -2,5 +2,9 @@
     echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/rknot/.zprofile
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/rknot/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-RED='\033[0;31m'
-printf "${RED}CLOSE YOUR TERMINAL AND OPEN IT AGAIN TO USE BREW${NC}\n"
+
+# Defining some colors
+RED='\033[1;91m'        # Red
+GREEN='\033[1;92m'      # Green
+
+printf "${GREEN}CLOSE YOUR TERMINAL AND OPEN IT AGAIN TO USE BREW${NC}\n"
--- make_backup.sh
+++ make_backup.sh
@@ -1,22 +1,25 @@
-#!/bin/bash
+#!/bin/zsh
 
+# finding brew path
+BREW=(`where brew`)
 # brew update; brew upgrade; brew cleanup
-brew update >&/dev/null
-brew upgrade >&/dev/null
-brew cleanup >&/dev/null
+# >&/dev/null
+$BREW update 
+$BREW upgrade
+$BREW cleanup
 # saving all formula and casks in backup.sh and making it executable
-/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' >./backup.sh
-chmod +x ./backup.sh >&/dev/null
+#/opt/homebrew/bin/brew list | awk '{print "echo installing "  $0 "\n" "brew install " $0 " >&/dev/null"}' >./backup.sh
+#chmod +x ./backup.sh >&/dev/null
 
 # backing up dot files that exist in the home directory
 # backing up oh-my-zsh files
 #rm -rf ./.oh-my-zsh >&/dev/null
 #mkdir .oh-my-zsh >&/dev/null
 #mkdir .oh-my-zsh/custom >&/dev/null
-cp ~/.oh-my-zsh/custom/aliases.zsh ./aliases.zsh >&/dev/null
-cp ~/.zshrc ./.zshrc >&/dev/null
-cp ~/.vimrc ./.vimrc >&/dev/null
-cp ~/.tmux.conf ./.tmux.conf >&/dev/null
-cp ~/.config/kitty/kitty.conf ./.kitty.conf >&/dev/null
-cp ~/.alacritty.yml ./.alacritty.yml >&/dev/null
-cp ~/.zprofile ./.zprofile >&/dev/null
+#cp ~/.oh-my-zsh/custom/aliases.zsh ./aliases.zsh >&/dev/null
+#cp ~/.zshrc ./.zshrc >&/dev/null
+#cp ~/.vimrc ./.vimrc >&/dev/null
+#cp ~/.tmux.conf ./.tmux.conf >&/dev/null
+#cp ~/.config/kitty/kitty.conf ./.kitty.conf >&/dev/null
+#cp ~/.alacritty.yml ./.alacritty.yml >&/dev/null
+#cp ~/.zprofile ./.zprofile >&/dev/null
