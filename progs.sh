7cd65aa96ef256cd8c4f69a9c41d3cfe730c71bd
adarijani <115781777+adarijani@users.noreply.github.com>
2023-03-11 07:41:51 +0100

    Update and rename brew.sh to progs.sh

--- progs.sh
+++ progs.sh
@@ -3,7 +3,7 @@
 sudo pacman -S vim --noconfirm
 sudo pacman -S kitty --noconfirm
 sudo pacman -S wezterm --noconfirm
-sudo pacman -S  --noconfirm
+sudo pacman -S lsd --noconfirm
 sudo pacman -S vim --noconfirm
 sudo pacman -S vim --noconfirm
 sudo pacman -S vim --noconfirm
