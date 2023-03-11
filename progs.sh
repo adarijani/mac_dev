7cf40be16c5c107d1b605964931f14968159615b
adarijani <115781777+adarijani@users.noreply.github.com>
2023-03-11 07:55:03 +0100

    Update progs.sh

--- progs.sh
+++ progs.sh
@@ -3,9 +3,9 @@
 sudo pacman -S vim --noconfirm
 sudo pacman -S kitty --noconfirm
 sudo pacman -S wezterm --noconfirm
-sudo pacman -S lsd --noconfirm
-sudo pacman -S vim --noconfirm
-sudo pacman -S vim --noconfirm
+sudo pacman -S fzf lsd --noconfirm
+sudo pacman -S most vim --noconfirm
+sudo pacman -S lsd vim --noconfirm
 sudo pacman -S vim --noconfirm
 sudo pacman -S vim --noconfirm
 sudo pacman -S vim --noconfirm
